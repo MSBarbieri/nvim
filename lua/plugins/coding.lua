8c5c22771c59abac701e15323641e0955286222e
MSBarbieri <matheussouzabarbieri@gmail.com>
2025-04-19 14:08:53 -0300

    foo

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -4,7 +4,14 @@ return {
     cmd = "Copilot",
     build = ":Copilot auth",
     opts = {
-      suggestion = { enabled = true },
+      suggestion = {
+        enabled = true,
+        keymap = {
+          accept = "<C-c>",
+          next = "<C-n>",
+          prev = "<C-p>",
+        },
+      },
       panel = { enabled = false },
       filetypes = {
         markdown = true,
